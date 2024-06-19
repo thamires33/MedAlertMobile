77d1988baf6d4b0aca31d4c29f16d94964f256d6
Vinicius <vinisousa.dsc@gmail.com>
2024-06-19 04:47:13 -0300

    Adição tela e estilização Alarme

---
+++ frontEnd/components/Alarm/styles.js
@@ -0,0 +1,74 @@
+import { StyleSheet } from 'react-native';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollView: {
+    flex: 1,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 16,
+    backgroundColor: '#f8f8f8', // Cor de fundo do cabeçalho
+  },
+  menuIconContainer: {
+    padding: 8,
+  },
+  headerTextContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  headerTextRegular: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  profileIconContainer: {
+    padding: 8,
+  },
+  profileIcon: {
+    width: 32,
+    height: 32,
+    borderRadius: 16,
+  },
+  card: {
+    margin: 16,
+    padding: 16,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 8,
+    backgroundColor: '#fff',
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#000',
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 16,
+    marginVertical: 8,
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    marginBottom: 16,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  halfContainer: {
+    flex: 1,
+    marginRight: 8,
+  },
+});
+export default styles;
