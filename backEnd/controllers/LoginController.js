c7d63387517550e6c7070ed5a46e555d284797f7
fabiocst <fabiocos996@gmail.com>
2024-06-21 18:37:20 -0300

    tentativa de configuração dos testes

--- backEnd/controllers/LoginController.js
+++ backEnd/controllers/LoginController.js
@@ -3,19 +3,22 @@ const router = express.Router();
 const Usuario = require('../models/Login');
 
 router.post('/', async (req, res) => {
-    const { email } = req.body;
-    const { senha } = req.body;
+    const { email, senha } = req.body;
 
     if (!email || !senha) {
         return res.status(400).json({ error: 'Por favor, forneça email e senha' });
     }
 
     try {
+        console.log(`Tentando login para email: ${email}`);
+
         const usuario = await Usuario.findOne({ where: { email, senha } });
 
         if (usuario) {
+            console.log('Usuário encontrado:', usuario);
             return res.status(200).json({ success: true, message: 'Login bem-sucedido' });
         } else {
+            console.log('Credenciais inválidas para email:', email);
             return res.status(401).json({ success: false, message: 'Credenciais inválidas' });
         }
     } catch (error) {
