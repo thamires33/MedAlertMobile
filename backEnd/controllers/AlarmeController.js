69b899e1476fbd6c2d25d23582c5ee8102a280b2
Vinicius <vinisousa.dsc@gmail.com>
2024-06-28 21:20:35 -0300

    Modificação do controller para funcionar de acordo com a nova model

--- backEnd/controllers/AlarmeController.js
+++ backEnd/controllers/AlarmeController.js
@@ -37,36 +37,4 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-// Deleta Alarme por ID (DELETE)
-router.delete('/:id', async (req, res) => {
-    try {
-        const rowsDeleted = await Alarme.destroy({ where: { id_alarme: req.params.id } });
-        if (rowsDeleted) {
-            res.status(200).json({ message: 'Alarme excluído com sucesso' });
-        } else {
-            res.status(404).json({ message: 'Alarme não encontrado' });
-        }
-    } catch (error) {
-        res.status(500).json({ message: 'Erro ao excluir alarme', error });
-    }
-});
-
-// Altera Alarme por ID (PUT)
-router.put('/:id', async (req, res) => {
-    try {
-        const { fk_usuario,nome_medicamento, posologia, intervalo_doses } = req.body;
-        const [rowsUpdated] = await Alarme.update(
-            { fk_medicamento,nome_medicamento, posologia, intervalo_doses },
-            { where: { id_alarme: req.params.id } }
-        );
-        if (rowsUpdated) {
-            res.status(200).json({ message: 'Alarme atualizado com sucesso' });
-        } else {
-            res.status(404).json({ message: 'Alarme não encontrado' });
-        }
-    } catch (error) {
-        res.status(500).json({ message: 'Erro ao atualizar alarme', error });
-    }
-});
-
 module.exports = router;
