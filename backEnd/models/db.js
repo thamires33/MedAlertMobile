d3749cc1abdc1a58c6c2083fba0792099d89ee6b
Vinicius <vinisousa.dsc@gmail.com>
2024-07-12 03:30:20 -0300

    Atualizações

--- backEnd/models/db.js
+++ backEnd/models/db.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
-//Conexão com o banco de dados
+
 const sequelize = new Sequelize('medalert', 'root', 'root', {
 host: "localhost",
 port: "3306",
 dialect: 'mysql'
 });
-//Vamos exportar as variáveis
+
 module.exports = {
 Sequelize: Sequelize,
 sequelize: sequelize
