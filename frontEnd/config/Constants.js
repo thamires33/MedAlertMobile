f8cecbd7ce41ea2ecb0a118eadd4b6c64912ca26
Vinicius <vinisousa.dsc@gmail.com>
2024-07-14 01:35:12 -0300

    Foi ignorado?

--- frontEnd/config/Constants.js
+++ frontEnd/config/Constants.js
@@ -1,2 +1,2 @@
-//export const apiEndpoint = "http://bota teu ip aqui:8081";
-export const apiEndpoint = "http://localhost:8081";
+export const apiEndpoint = "http://192.168.3.227:8081";
+//export const apiEndpoint = "http://localhost:8081";
