8e55ebfdbb9b2a4892db5ee77d0f280463bad0f6
http.thami <http.thami@gmail.com>
2024-07-22 22:44:46 +0000

    subindo alteracoes

--- frontEnd/App.js
+++ frontEnd/App.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from './components/Login/LoginScreen';
 import AlarmScreen from './components/Alarm/AlarmScreen';
 import CadastroScreen from './components/Cadastro/CadastroScreen';
+import ProfileScreen from './components/Profile/ProfileScreen';
 
 import { StatusBar } from 'expo-status-bar';
 import HomeScreen from './components/Home/HomeScreen';
@@ -22,6 +23,7 @@ export default function App() {
           <Stack.Screen name="Alarm" component={AlarmScreen} />
           <Stack.Screen name="Home" component={HomeScreen}/>
           <Stack.Screen name="Cadastro" component={CadastroScreen}/>
+          <Stack.Screen name="Profile" component={ProfileScreen}/>
         </Stack.Navigator>
         <StatusBar style="auto" />
       </NavigationContainer>
