0b6463b0bb0be2eead9bf746f1dd354f3d3b4c27
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2024-10-21 18:38:03 -0300

    Adicionando statics

--- django/medalert/urls.py
+++ django/medalert/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('alarmes.urls')),
     path('', include('medicamentos.urls')),
     path('', include('usuarios.urls')),
-] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    # re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+]
