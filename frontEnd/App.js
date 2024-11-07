464529c06cb54ca45b00aa9a263e162b64a05e7e
thamires33 <http.thami@gmail.com>
2024-11-07 20:15:26 -0300

    conflitos

--- frontEnd/App.js
+++ frontEnd/App.js
@@ -7,12 +7,6 @@ import { StatusBar } from 'expo-status-bar';
 import LoginScreen from './components/Login/LoginScreen';
 import AlarmScreen from './components/Alarm/AlarmScreen';
 import CadastroScreen from './components/Cadastro/CadastroScreen';
-<<<<<<< HEAD
-=======
-import ProfileScreen from './components/Profile/ProfileScreen';
-
-import { StatusBar } from 'expo-status-bar';
->>>>>>> 0b8fd46f02b0c189c4c7d6e80eff601c0ee3bd0a
 import HomeScreen from './components/Home/HomeScreen';
 import ProfileScreen from './components/Profile/ProfileScreen';
 
@@ -21,7 +15,6 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-<<<<<<< HEAD
       <Stack.Navigator
         initialRouteName="Login"
         screenOptions={{ headerShown: false }}
@@ -34,18 +27,5 @@ export default function App() {
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
-=======
-
-        <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Alarm" component={AlarmScreen} />
-          <Stack.Screen name="Home" component={HomeScreen}/>
-          <Stack.Screen name="Cadastro" component={CadastroScreen}/>
-          <Stack.Screen name="Profile" component={ProfileScreen}/>
-        </Stack.Navigator>
-        <StatusBar style="auto" />
-      </NavigationContainer>
->>>>>>> 0b8fd46f02b0c189c4c7d6e80eff601c0ee3bd0a
   );
 }
