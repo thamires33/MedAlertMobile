ba7c861390419f12ce8b60f0c2d91481a2feb4af
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2024-12-07 00:15:15 -0300

    Adicionando modal de apagar medicamento

--- frontEnd/components/Medicamentos/MedicamentosScreen.js
+++ frontEnd/components/Medicamentos/MedicamentosScreen.js
@@ -1,5 +1,13 @@
 import React, { useEffect, useState, useCallback } from "react";
-import { View, Text, Image, SafeAreaView, Keyboard, ScrollView, TouchableOpacity, BackHandler, Alert, Modal } from "react-native";
+import { 
+    View, 
+    Text, 
+    SafeAreaView, 
+    ScrollView, 
+    TouchableOpacity, 
+    Alert, 
+    Modal 
+} from "react-native";
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { useNavigation, useIsFocused, useFocusEffect } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -9,83 +17,99 @@ import { apiEndpoint, access_token } from "../../config/Constants";
 
 const MedicamentosScreen = () => {
     const navigation = useNavigation();
-    const [medicamentos, setMedicamentos] = useState([]);
     const isFocused = useIsFocused();
 
-    // Função para buscar os medicamentos do servidor
-    const fetchAllMedicamentos = async () => {
+    const [medicamentos, setMedicamentos] = useState([]);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [selectedMedicamentoId, setSelectedMedicamentoId] = useState(null);
+
+    // Busca os medicamentos do servidor
+    const fetchMedicamentos = async () => {
         try {
             const token = await AsyncStorage.getItem(access_token);
-            if (!token) {
-                throw new Error('Token não encontrado');
-            }
+            if (!token) throw new Error("Token não encontrado");
 
             const response = await axios.get(`${apiEndpoint}/medicamentos/`, {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
+                headers: { 'Authorization': `Bearer ${token}` },
             });
 
             setMedicamentos(response.data);
-            Keyboard.dismiss(); // Fecha o teclado caso esteja aberto
         } catch (err) {
-            console.log('Erro ao buscar medicamentos:', err);
-            if (err.response && err.response.status === 401) {
-                navigation.navigate('Login');
+            console.error("Erro ao buscar medicamentos:", err);
+            if (err.response?.status === 401) {
+                navigation.navigate("Login");
             }
         }
     };
 
-    // Busca medicamentos sempre que a tela está visível
+    // Deleta um medicamento pelo ID
+    const handleDeleteMedicamento = async (id) => {
+        try {
+            const token = await AsyncStorage.getItem(access_token);
+            if (!token) throw new Error("Token não encontrado");
+
+            const response = await axios.delete(`${apiEndpoint}/medicamentos/${id}/`, {
+                headers: { 'Authorization': `Bearer ${token}` },
+            });
+
+            if (response.status === 204) {
+                fetchMedicamentos();
+                setIsModalVisible(false);
+                Alert.alert("Sucesso", "Medicamento excluído com sucesso.");
+            }
+        } catch (err) {
+            console.error("Erro ao excluir medicamento:", err);
+            Alert.alert("Erro", "Não foi possível excluir o medicamento.");
+        }
+    };
+
+    // Atualiza a lista de medicamentos ao focar na tela
     useEffect(() => {
         if (isFocused) {
-            fetchAllMedicamentos();
+            fetchMedicamentos();
         }
     }, [isFocused]);
 
-    // Componente de renderização da lista de medicamentos
-    function MedicamentoCard({ data }) {
-        return (
-            <View style={styles.card}>
-                <Text style={styles.cardTitle}>{data.nome}</Text>
-                <Text style={styles.cardSubtitle}>
-                    <Text style={styles.boldText}>Dosagem:</Text> {data.dosagem} {data.unidade}
-                </Text>
-                <Text style={styles.cardSubtitle}>
-                    <Text style={styles.boldText}>Frequência:</Text> {data.frequencia} horas
-                </Text>
-                <Text style={styles.cardSubtitle}>
-                    <Text style={styles.boldText}>Data:</Text> {new Date(data.data).toLocaleDateString()}
-                </Text>
-                <Text style={styles.cardSubtitle}>
-                    <Text style={styles.boldText}>Horário:</Text> {data.horario}
-                </Text>
-                {/* {data.imagem && (
-                    <Image source={{ uri: data.imagem }} style={styles.cardImage} />
-                )} */}
+    // Exibe o modal de confirmação
+    const confirmDelete = (id) => {
+        setSelectedMedicamentoId(id);
+        setIsModalVisible(true);
+    };
+
+    // Componente de renderização do medicamento
+    const MedicamentoCard = ({ data }) => (
+        <View style={styles.card}>
+            <Text style={styles.cardTitle}>{data.nome}</Text>
+            <Text style={styles.cardSubtitle}>
+                <Text style={styles.boldText}>Dose:</Text> {data.dose}
+            </Text>
+            <Text style={styles.cardSubtitle}>
+                <Text style={styles.boldText}>Intervalo:</Text> {data.intervalo_horas} horas
+            </Text>
+            <Text style={styles.cardSubtitle}>
+                <Text style={styles.boldText}>Recomendação:</Text> {data.recomendacao || "Nenhuma recomendação"}
+            </Text>
+            <View style={styles.cardButtonContainer}>
+                <TouchableOpacity
+                    style={[styles.excludeButton, styles.halfWidthButton]}
+                    onPress={() => confirmDelete(data.id)}
+                >
+                    <Text style={styles.textButton}>Excluir Medicamento</Text>
+                </TouchableOpacity>
             </View>
-        );
-    }
+        </View>
+    );
 
     return (
         <View style={styles.container}>
             <SafeAreaView style={styles.safeArea}>
                 <View style={styles.header}>
-                    {/* <TouchableOpacity onPress={() => navigation.openDrawer()} style={styles.menuIconContainer}>
-                        <Icon name="menu" size={24} color="#000" />
+                    {/* <TouchableOpacity onPress={() => navigation.goBack()} style={styles.menuIconContainer}>
+                        <Icon name="arrow-back" size={24} color="#000" />
                     </TouchableOpacity> */}
                     <View style={styles.headerTextContainer}>
                         <Text style={styles.headerTextRegular}>Medicamentos</Text>
                     </View>
-                    <TouchableOpacity
-                        style={styles.profileIconContainer}
-                        onPress={() => navigation.navigate('Profile')}
-                    >
-                        <Image
-                            source={{ uri: 'https://via.placeholder.com/150' }}
-                            style={styles.profileIcon}
-                        />
-                    </TouchableOpacity>
                 </View>
 
                 <ScrollView contentContainerStyle={styles.scrollViewContentContainer}>
@@ -100,14 +124,33 @@ const MedicamentosScreen = () => {
                     )}
                 </ScrollView>
 
-                <View style={styles.buttonContainer}>
-                    <TouchableOpacity
-                        style={styles.addButton}
-                        onPress={() => navigation.navigate('Cadastrar medicamento')}
-                    >
-                        <Text style={styles.txtAddButton}>Adicionar Medicamento</Text>
-                    </TouchableOpacity>
-                </View>
+                {/* Modal de confirmação */}
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={isModalVisible}
+                    onRequestClose={() => setIsModalVisible(false)}
+                >
+                    <View style={styles.modalContainer}>
+                        <View style={styles.modalContent}>
+                            <Text style={styles.modalText}>Tem certeza que deseja excluir este medicamento?</Text>
+                            <View style={styles.modalButtonContainer}>
+                                <TouchableOpacity
+                                    style={[styles.modalButton, styles.modalCancelButton]}
+                                    onPress={() => setIsModalVisible(false)}
+                                >
+                                    <Text style={styles.modalButtonText}>Cancelar</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity
+                                    style={[styles.modalButton, styles.modalConfirmDeleteButton]}
+                                    onPress={() => handleDeleteMedicamento(selectedMedicamentoId)}
+                                >
+                                    <Text style={styles.modalButtonText}>Confirmar</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
             </SafeAreaView>
         </View>
     );
